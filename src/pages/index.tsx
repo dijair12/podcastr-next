4449bd0e292394c7bfc27d3de7b64a083442fee6
dijair12 <dijair.ribeiro@aluno.faculdadeimpacta.com.br>
2021-05-10 21:17:43 -0300

    final components Header Player v2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes)
   return (
-    <h1>Hello World!</h1>
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+
+  return {
+    props: {
+      episodes: await response.json(),
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
